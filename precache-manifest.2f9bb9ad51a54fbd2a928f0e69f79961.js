6f9c012c8c73811c3aba91791f8e2460a2735396
Erick Silva <erick.silva6653@gmail.com>
2021-08-31 14:59:07 -0300

    Updates

---
+++ precache-manifest.2f9bb9ad51a54fbd2a928f0e69f79961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f804ab071fb54a89e13c4722206b8e",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "f8dbb65211843d03b31e",
+    "url": "/trivia-game/static/css/main.8cada77a.chunk.css"
+  },
+  {
+    "revision": "110fb91ed106db7b0a93",
+    "url": "/trivia-game/static/js/2.38bf1f29.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/trivia-game/static/js/2.38bf1f29.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8dbb65211843d03b31e",
+    "url": "/trivia-game/static/js/main.3e7500b3.chunk.js"
+  },
+  {
+    "revision": "28c450d90070e1cbeaea",
+    "url": "/trivia-game/static/js/runtime-main.34810379.js"
+  }
+]);
